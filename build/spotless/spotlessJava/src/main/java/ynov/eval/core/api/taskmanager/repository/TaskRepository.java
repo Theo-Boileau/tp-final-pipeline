e7ccd690f371d5157b889cf10bd2d8bdfc1ab228
Theo BOILEAU <theo.boileau@ynov.com>
2023-04-27 14:59:53 +0200

    Fix: nexus properties

--- build/spotless/spotlessJava/src/main/java/ynov/eval/core/api/taskmanager/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package ynov.eval.core.api.taskmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ynov.eval.core.api.taskmanager.model.Task;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Integer> {}
